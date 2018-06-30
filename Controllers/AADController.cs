4ba05f890d8231483ad21d89f779c27eb987e30a
Michael Hansen <mihansen@microsoft.com>
2018-06-29 21:05:32 -0400

    Initial commit

---
+++ Controllers/AADController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace SmartOnFHIR_AAD_Proxy.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class AADController : ControllerBase
+    {
+        
+        private IConfiguration _configuration;
+
+        public AADController(IConfiguration config)
+        {
+            _configuration = config;
+
+            if (String.IsNullOrEmpty(_configuration["AadHostName"]))
+            {
+                AadHostName = "login.microsoftonline.com";
+            }
+        }
+
+        private string AadHostName { get; }
+
+        //V1 Authorize Endpoint
+        [HttpGet("{tenant}/oauth2/authorize")]
+        public ActionResult GetV1(string tenant, 
+                                [FromQuery]string response_type,
+                                [FromQuery]string client_id,
+                                [FromQuery]string redirect_uri,
+                                [FromQuery]string launch,
+                                [FromQuery]string scope,
+                                [FromQuery]string state,
+                                [FromQuery]string aud)
+        {
+            string newQueryString = $"resource={aud}&response_type={response_type}&redirect_uri={redirect_uri}&client_id={client_id}";
+
+            if (!String.IsNullOrEmpty(launch)) {
+                //TODO: Implement appropriate behavior
+            }
+
+            if (!String.IsNullOrEmpty(state)) {
+                newQueryString += $"state={state}";
+            }
+
+            return RedirectPermanent($"https://{AadHostName}/{tenant}/oauth2/authorize?{newQueryString}");
+        }
+
+        //V2 Authorize endpoint
+        [HttpGet("{tenant}/oauth2/v2.0/authorize")]
+        public ActionResult GetV2(string tenant, 
+                                [FromQuery]string response_type,
+                                [FromQuery]string client_id,
+                                [FromQuery]string redirect_uri,
+                                [FromQuery]string launch,
+                                [FromQuery]string scope,
+                                [FromQuery]string state,
+                                [FromQuery]string aud)
+        {
+            string newQueryString = $"response_type={response_type}&redirect_uri={redirect_uri}&client_id={client_id}";
+
+            if (!String.IsNullOrEmpty(launch)) {
+                //TODO: Implement appropriate behavior
+            }
+
+            if (!String.IsNullOrEmpty(state)) {
+                newQueryString += $"&state={state}";
+            }
+
+            if (!String.IsNullOrEmpty(scope)) {
+                String[] scopes = scope.Split(' ');
+                var scopeString = "";
+                foreach (var s in scopes) {
+                    var newScope = s.Replace("/","-");
+                    scopeString += $"{aud}/{newScope} ";
+                }
+                newQueryString += $"&scope={scopeString}";
+            }
+
+            return RedirectPermanent($"https://{AadHostName}/{tenant}/oauth2/v2.0/authorize?{newQueryString}");
+        }
+
+        //V1 and V2 token endpoints
+        [HttpPost("{tenant}/oauth2/token")]
+        [HttpPost("{tenant}/oauth2/v2.0/token")]
+        public async Task<ActionResult> Post(string tenant,
+                                            [FromForm]string grant_type,
+                                            [FromForm]string code,
+                                            [FromForm]string redirect_uri,
+                                            [FromForm]string client_id,
+                                            [FromForm]string client_secret)
+        {
+            var client = new HttpClient();
+
+            client.BaseAddress = new Uri($"https://{AadHostName}");
+
+            var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type", grant_type),
+                new KeyValuePair<string, string>("code", code),
+                new KeyValuePair<string, string>("redirect_uri", redirect_uri),
+                new KeyValuePair<string, string>("client_id", client_id),
+                new KeyValuePair<string, string>("client_secret", client_secret)
+            });
+
+            var response = await client.PostAsync($"{Request.Path}", content);
+           
+            return new ContentResult() {
+                Content = await response.Content.ReadAsStringAsync(),
+                StatusCode = (int)response.StatusCode,
+                ContentType = "application/json"
+            };
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,49 @@
+Azure Active Directory Proxy for SMART on FHIR
+----------------------------------------------
+
+[Azure Active Directory](https://azure.microsoft.com/en-us/services/active-directory/) supports [OAuth2 authorization flow](https://docs.microsoft.com/en-us/azure/active-directory/develop/active-directory-protocols-oauth-code), however, some of the query parameter naming conventions are not compatible with [SMART on FHIR authorization](http://docs.smarthealthit.org/authorization/).
+
+Specifically SMART on FHIR expects to initiate the authorization with something like:
+
+```
+GET https://auth-server/authorize?aud=https://fhir-server&client_id=XXX&scope=patient/*.read&...
+```
+
+Azure Active Directory offers [two version](https://docs.microsoft.com/en-us/azure/active-directory/develop/active-directory-v2-compare) of the OAuth endpoint. Version 1 expects the `aud` (audience) parameter to be supplied as `resource` and in version 2, the audience is included in the fully qualified scope specifications. Specifically, if `aud=https://fhir-server` and `scope=patient/*.read`, the version 2 endpoint would expect no `aud` or `resource` parameter but the `scope` be fully qualified as `https://fhir-server/patient/*.read`. Moreover, the version 2 endpoint does not accept scopes with `/` (slash) in the name of the scope. All of these small differences makes it challenging to use Azure Active Directory in SMART on FHIR applications. 
+
+This repository contains a small ASP.NET Core app that will act as a proxy in front of Azure Active Directory and translate the parameter specifications provided by a SMART on FHIR application into something that Azure Active directory can accept. 
+
+You can deploy this app in an [Azure Web App](https://azure.microsoft.com/en-us/services/app-service/web/), or run it locally if you have .NET Core 2.1 installed. Run it locally with:
+
+```
+dotnet run
+```
+
+If you then initiate an autorization flow with:
+
+```
+GET http://localhost:5000/oauth2/v2.0/authorize?aud=https://fhir-server&client_id=XXX&scope=patient/*.read&...
+```
+
+It will effectively get translated into:
+
+```
+GET https://login.microsoftonline.com/oauth2/v2.0/authorize?client_id=XXX&scope=https://fhir-server/patient-*.read&...
+```
+
+Notice that the scope has been renamed with a hyphen and is now fully qualified. The app also supports the version 1 endpoint, in which case a request like:
+
+```
+GET http://localhost:5000/oauth2/authorize?aud=https://fhir-server&client_id=XXX&scope=patient/*.read&...
+```
+
+Would get translated to:
+
+```
+GET https://login.microsoftonline.com/oauth2/authorize?resource=https://fhir-server&client_id=XXX&...
+```
+
+The version 1 endpoint ignores scopes, they must be statically set when configuring the client application. 
+
+Tokens can be obtained from the token endpoint (e.g. `http://localhost:5000/oauth2/v2.0/token`). The proxy performs a *redirect* when using the `authorize` endpoint, but since the token request is a `POST` operation, the app acts as a *proxy* for the request.
+
